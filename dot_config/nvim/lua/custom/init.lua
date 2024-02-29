c4c2fe24ebaa62621bcf2fd613b38b1bd4c1b4b8
James Williams <james@kananlabs.org>
2024-02-29 11:37:24 -0800

    Add back swapfile and adjust default fold.

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,17 +1,25 @@
--- Disable swapfiles
-vim.opt.swapfile = false
+-- Enable autoread
+vim.opt.autoread = true
+
+-- Enable swapfiles
+vim.opt.swapfile = true
+
+-- Indenting
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
 -- Treesitter folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 3
+vim.opt.foldlevel = 99
 
 -- Auto format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
 })
 
 -- Session save and restore items
@@ -19,21 +27,21 @@ vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,
 
 -- GUI specific
 if vim.loop.os_uname().sysname == "Windows_NT" then
-	vim.o.guifont = "Hack Nerd Font:h10"
-	local powershell_options = {
-		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-		shellquote = "",
-		shellxquote = "",
-	}
+  vim.o.guifont = "Hack Nerd Font:h10"
+  local powershell_options = {
+    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
 
-	for option, value in pairs(powershell_options) do
-		vim.opt[option] = value
-	end
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 else
-	vim.o.guifont = "Hack Nerd Font:h13"
+  vim.o.guifont = "Hack Nerd Font:h13"
 end
 
 vim.g.neovide_cursor_animation_length = 0.01
