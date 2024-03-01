1fd24fe538641412424422fb5a35161cfd84e2f2
James Williams <james@kananlabs.org>
2024-03-01 13:17:54 -0800

    Fixed indentation.

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -2,54 +2,54 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require("custom.highlights")
+local highlights = require "custom.highlights"
 
 M.ui = {
-	theme = "everblush",
-	theme_toggle = { "everblush", "one_light" },
+  theme = "everblush",
+  theme_toggle = { "everblush", "one_light" },
 
-	hl_override = highlights.override,
-	hl_add = highlights.add,
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 
-	statusline = {
-		theme = "default", -- default/vscode/vscode_colored/minimal
-		-- default/round/block/arrow separators work only for default statusline theme
-		-- round and block will work for minimal theme only
-		separator_style = "arrow",
-		overriden_modules = function(modules)
-			table.insert(
-				modules,
-				2,
-				(function()
-					local success, path = pcall(function()
-						return vim.api.nvim_buf_get_name(0):match("^.*[/\\]")
-					end)
-					if success and path then
-						return "%#St_file_info#" .. " " .. path .. "%#St_file_sep#"
-					else
-						return ""
-					end
-				end)()
-			)
-		end,
-	},
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "arrow",
+    overriden_modules = function(modules)
+      table.insert(
+        modules,
+        2,
+        (function()
+          local success, path = pcall(function()
+            return vim.api.nvim_buf_get_name(0):match "^.*[/\\]"
+          end)
+          if success and path then
+            return "%#St_file_info#" .. " " .. path .. "%#St_file_sep#"
+          else
+            return ""
+          end
+        end)()
+      )
+    end,
+  },
 
-	nvdash = {
-		load_on_startup = true,
-		buttons = {
-			{ "  Find File", "Spc f f", "Telescope find_files" },
-			{ "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-			{ "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-			{ "  Bookmarks", "Spc m a", "Telescope marks" },
-			{ "  Themes", "Spc t h", "Telescope themes" },
-			{ "  Mappings", "Spc c h", "NvCheatsheet" },
-		},
-	},
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require("custom.mappings")
+M.mappings = require "custom.mappings"
 
 return M
--- dot_config/nvim/lua/custom/configs/overrides.lua
+++ dot_config/nvim/lua/custom/configs/overrides.lua
@@ -1,97 +1,97 @@
 local M = {}
 
 M.treesitter = {
-	ensure_installed = {
-		"vim",
-		"lua",
-		"html",
-		"css",
-		"javascript",
-		"typescript",
-		"tsx",
-		"c",
-		"markdown",
-		"markdown_inline",
-		"python",
-		"rust",
-		"json",
-	},
-	indent = {
-		enable = true,
-		-- disable = {
-		--   "python"
-		-- },
-	},
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "rust",
+    "json",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
 }
 
 M.mason = {
-	ensure_installed = {
-		-- lua stuff
-		"lua-language-server",
-		"stylua",
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
 
-		-- web dev stuff
-		"css-lsp",
-		"html-lsp",
-		"typescript-language-server",
-		"deno",
-		"prettier",
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
 
-		-- c/cpp stuff
-		"clangd",
-		"clang-format",
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
 
-		-- python stuff
-		"pyright",
-		"ruff-lsp",
-		"debugpy",
+    -- python stuff
+    "pyright",
+    "ruff-lsp",
+    "debugpy",
 
-		-- rust stuff
-		"rust-analyzer",
-	},
+    -- rust stuff
+    "rust-analyzer",
+  },
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
+  git = {
+    enable = true,
+  },
 
-	view = {
-		width = 40,
-	},
+  view = {
+    width = 40,
+  },
 
-	renderer = {
-		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "pyproject.toml", "tox.ini" },
-		highlight_git = false,
-		icons = {
-			git_placement = "after",
-			show = {
-				git = true,
-			},
-			glyphs = {},
-		},
-	},
-	filters = {
-		git_ignored = false,
-	},
+  renderer = {
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "pyproject.toml", "tox.ini" },
+    highlight_git = false,
+    icons = {
+      git_placement = "after",
+      show = {
+        git = true,
+      },
+      glyphs = {},
+    },
+  },
+  filters = {
+    git_ignored = false,
+  },
 }
 
 M.nvterm = {
-	terminals = {
-		type_opts = {
-			horizontal = { location = "rightbelow", split_ratio = 0.4 },
-			vertical = { location = "rightbelow", split_ratio = 0.4 },
-		},
-	},
-	behavior = {
-		autoclose_on_quit = {
-			enabled = true,
-			confirm = true,
-		},
-		close_on_exit = true,
-		auto_insert = true,
-	},
+  terminals = {
+    type_opts = {
+      horizontal = { location = "rightbelow", split_ratio = 0.4 },
+      vertical = { location = "rightbelow", split_ratio = 0.4 },
+    },
+  },
+  behavior = {
+    autoclose_on_quit = {
+      enabled = true,
+      confirm = true,
+    },
+    close_on_exit = true,
+    auto_insert = true,
+  },
 }
 
 return M
