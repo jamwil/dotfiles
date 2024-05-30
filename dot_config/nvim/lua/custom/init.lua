e85069446714503b8c70abf119d5ae37d486846b
James Williams <james@kananlabs.org>
2024-05-30 13:20:40 -0600

    Update neovide config.

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -10,21 +10,21 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-  end,
+	pattern = "lua",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
 
 -- Wrapping
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    -- Will create actual line breaks
-    vim.opt_local.textwidth = 80
-  end,
+	pattern = "markdown",
+	callback = function()
+		-- Will create actual line breaks
+		vim.opt_local.textwidth = 80
+	end,
 })
 
 -- Treesitter folding
@@ -34,10 +34,10 @@ vim.opt.foldlevel = 99
 
 -- Auto format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format { bufnr = args.buf }
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
 -- Session save and restore items
@@ -45,24 +45,24 @@ vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,
 
 -- GUI specific
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.o.guifont = "Hack Nerd Font:h10"
-  local powershell_options = {
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-  }
+	vim.o.guifont = "Hack Nerd Font:h10"
+	local powershell_options = {
+		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+		shellquote = "",
+		shellxquote = "",
+	}
 
-  for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-  end
+	for option, value in pairs(powershell_options) do
+		vim.opt[option] = value
+	end
 else
-  vim.o.guifont = "Hack Nerd Font:h13"
+	vim.o.guifont = "Hack Nerd Font:h13"
 end
 
 vim.g.neovide_cursor_animation_length = 0.01
 vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_scroll_animation_length = 0.05
-vim.g.neovide_input_macos_alt_is_meta = true
+vim.g.neovide_input_macos_option_key_is_meta = "both"
