2b13284e908b1257b72d618d0d9b7f3c3e7a2175
James Williams <jamwil@gmail.com>
2024-08-08 10:03:19 -0700

    Disable signs for pyright.

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -20,6 +20,7 @@ lspconfig["pyright"].setup {
   handlers = {
     ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = false,
+      signs = false,
     }),
   },
 }
