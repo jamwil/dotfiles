dc79fa16e2ced059e2ccb9464f685093070c57c4
James Williams <james.c.williams@cbre.com>
2025-03-27 18:33:41 -0700

    Add terraform config.

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "clangd", "ruff", "rust_analyzer", "pest_ls" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "ruff", "rust_analyzer", "pest_ls", "terraformls", "tflint" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
